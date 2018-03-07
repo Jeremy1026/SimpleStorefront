36b90ef57b063609b8a037daa7869ebb198e16f9
Jeremy Curcio <j.curcio@me.com>
2018-03-07 09:43:24 -0500

    Add Sweet Tea Item
    Signed-off-by: Jeremy Curcio <j.curcio@me.com>

---
+++ src/NoInc/SimpleStorefrontBundle/DataFixtures/ORM/LoadSweetTeaData.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace NoInc\SimpleStorefrontBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use NoInc\SimpleStorefrontBundle\Entity\User;
+use NoInc\SimpleStorefrontBundle\Entity\Ingredient;
+use NoInc\SimpleStorefrontBundle\Entity\Recipe;
+use NoInc\SimpleStorefrontBundle\Entity\RecipeIngredient;
+
+class LoadSweetTeaData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $ingredients = [];
+        foreach ( $this->ingredientArray() as $ingredientData )
+        {
+            $ingredient = new Ingredient();
+            
+            $ingredient->setName($ingredientData["name"]);
+            $ingredient->setPrice($ingredientData["price"]);
+            $ingredient->setMeasure($ingredientData["measure"]);
+            $ingredient->setStock(100);
+    
+            $manager->persist($ingredient);
+        
+            $ingredients[$ingredient->getName()] = $ingredient;
+        }
+        $manager->flush();
+        
+        $recipeData = $this->recipeArray();
+        
+        $recipe = new Recipe();
+        $recipe->setName($recipeData["name"]);
+        $recipe->setPrice($recipeData["price"]);
+        $manager->persist($recipe);
+        $manager->flush();
+        
+        foreach( $recipeData["ingredients"] as $recipeIngredientData )
+        {
+            $recipeIngredient = new RecipeIngredient();
+            
+            $recipeIngredient->setIngredient($ingredients[$recipeIngredientData["name"]]);
+            $recipeIngredient->setRecipe($recipe);
+            $recipeIngredient->setQuantity($recipeIngredientData["quantity"]);
+            $manager->persist($recipeIngredient);
+        }
+        $manager->flush();
+    }
+    
+    public function ingredientArray()
+    {
+        return [
+            [
+                "name" => "Tea Bags",
+                "price" => 0.35,
+                "measure" => "Count"
+            ],
+            [
+                "name" => "Sugar",
+                "price" => 0.75,
+                "measure" => "Cup"
+            ],
+            [
+                "name" => "Water",
+                "price" => 0.00,
+                "measure" => "Cup"
+            ]
+        ];
+    }
+    
+    public function recipeArray()
+    {
+        return [
+            "name" => "Sweet Tea",
+            "price" => 2,
+            "ingredients" => [
+                [
+                    "name" => "Tea Bags",
+                    "quantity" => 3
+                ],
+                [
+                    "name" => "Sugar",
+                    "quantity" => 2
+                ],
+                [
+                    "name" => "Water",
+                    "quantity" => 4
+                ],
+            ]
+        ];
+    }
+    
+    public function getOrder()
+    {
+        return 2;
+    }
+}
